d0e3a66e9e19acf85303949018297940d2b9221d
wahyualfarisi <wahyualfarisi30@gmail.com>
2021-05-28 11:19:11 +0700

    create content dashboard

--- src/containers/CoBrand/Dashboard/Dashboard.js
+++ src/containers/CoBrand/Dashboard/Dashboard.js
@@ -66,21 +66,113 @@ function Dashboard() {
 
             {/* Content */}
             <h1>CONTENT LIST</h1>
-            {/* <div className="Dashboard__contents">
-                <div className="Dashboard__contents__list">
-                    <img 
-                        src={'https://images.unsplash.com/photo-1573246123716-6b1782bfc499?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1560&q=80'} 
-                        alt="Card photo 1" 
-                        className="Dashboard__contents__list-img"
-                    />
-                    <h3>Mangga Senyum Menarik</h3>
+            <div className="Dashboard__contents">
+                <div className="Dashboard__contents__item">
+                    <div className="Dashboard__contents__cover">
+                        <img 
+                            src={'https://images.unsplash.com/photo-1573246123716-6b1782bfc499?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1560&q=80'} 
+                            alt="Card photo 1" 
+                            className="Dashboard__contents__cover-img"
+                        />
+                        <div className="Dashboard__contents__shadow"></div>
+                    </div>
                     <div className="Dashboard__contents__description">
+                        <h3>Mangga Senyum Menarik</h3>
                         <p>
                         is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
                         </p>
                     </div>
                 </div>
-            </div> */}
+
+                <div className="Dashboard__contents__item">
+                    <div className="Dashboard__contents__cover">
+                        <img 
+                            src={'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1502&q=80'} 
+                            alt="Card photo 1" 
+                            className="Dashboard__contents__cover-img"
+                        />
+                        <div className="Dashboard__contents__shadow"></div>
+                    </div>
+                    <div className="Dashboard__contents__description">
+                        <h3>Burger mengandung banyak kegunaan </h3>
+                        <p>
+                        is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                        </p>
+                    </div>
+                </div>
+
+
+                <div className="Dashboard__contents__item">
+                    <div className="Dashboard__contents__cover">
+                        <img 
+                            src={'https://images.unsplash.com/photo-1534080564583-6be75777b70a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80'} 
+                            alt="Card photo 1" 
+                            className="Dashboard__contents__cover-img"
+                        />
+                        <div className="Dashboard__contents__shadow"></div>
+                    </div>
+                    <div className="Dashboard__contents__description">
+                        <h3>Enaknya menyantap seafood di bulan puasa</h3>
+                        <p>
+                        is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                        </p>
+                    </div>
+                </div>
+
+                <div className="Dashboard__contents__item">
+                    <div className="Dashboard__contents__cover">
+                        <img 
+                            src={'https://images.unsplash.com/photo-1621570169694-4867389dcc66?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80'} 
+                            alt="Card photo 1" 
+                            className="Dashboard__contents__cover-img"
+                        />
+                        <div className="Dashboard__contents__shadow"></div>
+                    </div>
+                    <div className="Dashboard__contents__description">
+                        <h3>Cinta menyatukan kita</h3>
+                        <p>
+                        is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                        </p>
+                    </div>
+                </div>
+
+                <div className="Dashboard__contents__item">
+                    <div className="Dashboard__contents__cover">
+                        <img 
+                            src={'https://images.unsplash.com/photo-1509966756634-9c23dd6e6815?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1010&q=80'} 
+                            alt="Card photo 1" 
+                            className="Dashboard__contents__cover-img"
+                        />
+                        <div className="Dashboard__contents__shadow"></div>
+                    </div>
+                    <div className="Dashboard__contents__description">
+                        <h3>Ayo belajar</h3>
+                        <p>
+                        is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                        </p>
+                    </div>
+                </div>
+
+                <div className="Dashboard__contents__item">
+                    <div className="Dashboard__contents__cover">
+                        <img 
+                            src={'https://images.unsplash.com/photo-1529156069898-49953e39b3ac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2689&q=80'} 
+                            alt="Card photo 1" 
+                            className="Dashboard__contents__cover-img"
+                        />
+                        <div className="Dashboard__contents__shadow"></div>
+                    </div>
+                    <div className="Dashboard__contents__description">
+                        <h3>Kebersamaan di tengah pandemic</h3>
+                        <p>
+                        is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                        </p>
+                    </div>
+                </div>
+
+                
+                
+            </div>
 
         </div>
     )
