6f1790181a4e770c6a7d5de321eed85c694eb5f2
wahyualfarisi <wahyualfarisi30@gmail.com>
2021-05-31 09:46:47 +0700

    form add program

--- src/containers/CoBrand/Program/AddProgram/AddProgram.js
+++ src/containers/CoBrand/Program/AddProgram/AddProgram.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import './AddProgram.scss';
 
 function AddProgram() {
     return (
-        <div>
-            <h1>ADD PROGRAM</h1>
+        <div className="AddProgram">
+            <h1>Create New Program</h1>
+            <div className="form-group">
+                <label>Category</label>
+                <select>
+                    <option>Artikel</option>
+                    <option>Lomba</option>
+                </select>
+            </div>
+            <div className="form-group">
+                <label>Title</label>
+                <input 
+                    type="text" 
+                    className="form-group__input form-group__input--fullwidth" 
+                    placeholder="Example"
+                />
+            </div>
+            <div className="form-group">
+                <label>Description</label>
+                <textarea placeholder="Type Something...">
+
+                </textarea>
+            </div>
+            <div className="form-group">
+                <label>Attachment</label>
+                <input type="file" className="form-group__input" />
+            </div>
+            <div className="form-group">
+                <label>Set Schedule</label>
+                <input type="date" className="form-group__input" />
+            </div>
         </div>
     )
 }
