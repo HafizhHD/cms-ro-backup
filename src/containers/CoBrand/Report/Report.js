25bd38103c885de2812b301590bfaa606f40d374
wahyualfarisi <wahyualfarisi30@gmail.com>
2021-06-02 13:53:01 +0700

    create subscriber chart

--- src/containers/CoBrand/Report/Report.js
+++ src/containers/CoBrand/Report/Report.js
@@ -7,13 +7,12 @@ function Report() {
 
     
 
-    const [activeSubsState, setActiveSubsState] = useState({
+    const [activeSubsState] = useState({
         series: [83],
         options: {
             chart: {
                 type: 'radialBar',
             },
-          
             plotOptions: {
             radialBar: {
                 hollow: {
@@ -39,7 +38,7 @@ function Report() {
                     show: true,
                     value: {
                         show: true,
-                        fontSize: '4.5rem',
+                        fontSize: '3.5rem',
                         fontFamily: undefined,
                         fontWeight: 400,
                         color: '#1e917b',
@@ -53,8 +52,128 @@ function Report() {
             },
             colors: ['#1e917b'],
             labels: [''],
+        }
+    });
+
+    const [newSubsState] = useState({
+        series: [{
+            name: "Desktops",
+            data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+        }],
+        options: {
+          chart: {
+            height: 350,
+            type: 'line',
+            zoom: {
+              enabled: false
+            }
+          },
+          colors: ['#1e917b'],
+          dataLabels: {
+            enabled: false
+          },
+          stroke: {
+            curve: 'smooth'
+          },
+          title: {
+            text: '',
+            align: 'left'
+          },
+          grid: {
+            row: {
+              colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+              opacity: 0.5
+            },
+          },
+          xaxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+          }
         },
-        
+    });
+
+    const [subsState] = useState({
+        series: [{
+            name: 'Inflation',
+            data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2]
+          }],
+          options: {
+            chart: {
+              height: 350,
+              type: 'bar',
+              width: '100%'
+            },
+            colors: ['#1e917b'],
+            plotOptions: {
+              bar: {
+                borderRadius: 10,
+                columnWidth: '60%',
+                dataLabels: {
+                  position: 'top', // top, center, bottom
+                },
+              }
+            },
+            dataLabels: {
+              enabled: true,
+              formatter: function (val) {
+                return val + "%";
+              },
+              offsetY: -20,
+              style: {
+                fontSize: '12px',
+                colors: ["#304758"]
+              }
+            },
+            
+            xaxis: {
+              categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+              position: 'top',
+              axisBorder: {
+                show: false
+              },
+              axisTicks: {
+                show: false
+              },
+              crosshairs: {
+                fill: {
+                  type: 'gradient',
+                  gradient: {
+                    colorFrom: '#D8E3F0',
+                    colorTo: '#BED1E6',
+                    stops: [0, 100],
+                    opacityFrom: 0.4,
+                    opacityTo: 0.5,
+                  }
+                }
+              },
+              tooltip: {
+                enabled: true,
+              }
+            },
+            yaxis: {
+              axisBorder: {
+                show: false
+              },
+              axisTicks: {
+                show: false,
+              },
+              labels: {
+                show: false,
+                formatter: function (val) {
+                  return val + "%";
+                }
+              }
+            
+            },
+            title: {
+              text: '',
+              floating: true,
+              offsetY: 330,
+              align: 'center',
+              style: {
+                color: '#444'
+              }
+            }
+          }
     })
 
 
@@ -81,7 +200,9 @@ function Report() {
                     <FiInfo className="card_report_heading-icon" />
                 </div>
                 <div className="card_report_content">
-                    New Subscriber
+                    <div>
+                        <ApexChart options={newSubsState.options} series={newSubsState.series} height={250} />
+                    </div>
                 </div>
             </div>
             <div className="Report__subscriber card_report">
@@ -90,7 +211,9 @@ function Report() {
                     <FiInfo className="card_report_heading-icon" />
                 </div>
                 <div className="card_report_content">
-                    New Subscriber
+                    <div>
+                        <ApexChart options={subsState.options} series={subsState.series} type="bar" height={350} />
+                    </div>
                 </div>
             </div>
 
