80b1ba9be246f1ae71052c78534556d45a26021f
wahyualfarisi <wahyualfarisi30@gmail.com>
2021-07-07 17:51:42 +0700

    update server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 8085;
+const port = 5000;
 // const port = 5000;
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
