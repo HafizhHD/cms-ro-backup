afbe4ea91617b156801f8bcbad5d3e723e9c09f2
hafizhdary <hafizh.dary@defghi.id>
2021-09-14 11:51:23 +0700

    update port for server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3333;
-// const port = 5000;
+//const port = 3000;
+const port = 5000;
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
