04dece8ae04ba6ddb2ea062ad08fc7620be5210f
Nafis <nafis@IDE2SENs-iMac.local>
2021-12-20 14:30:49 +0700

    update

--- src/containers/CoBrand/Content/AddContent/AddContent.js
+++ src/containers/CoBrand/Content/AddContent/AddContent.js
@@ -25,56 +25,6 @@ function AddContent({
     isLoading
 }) {
 
-    //pdf
-    // Create new plugin instance
-    const defaultLayoutPluginInstance = defaultLayoutPlugin();
-
-    // for onchange event
-    const [pdfFile, setPdfFile] = useState(null);
-    const [pdfFileError, setPdfFileError] = useState('');
-
-    // for submit event
-    const [viewPdf, setViewPdf] = useState(null);
-
-    // onchange event
-    const fileType = ['application/pdf'];
-    const handlePdfFileChange = (e) => {
-        let selectedFile = e.target.files[0];
-        if (selectedFile) {
-            if (selectedFile && fileType.includes(selectedFile.type)) {
-                let reader = new FileReader();
-                reader.readAsDataURL(selectedFile);
-                reader.onloadend = (e) => {
-                    setPdfFile(e.target.result);
-                    setPdfFileError('');
-                    // handleChange()
-                    console.log(pdfFile) //null
-                    console.log(e.target.result)
-                    
-                }
-            }
-            else {
-                setPdfFile(null);
-                setPdfFileError('Please select valid pdf file');
-            }
-        }
-        else {
-            console.log('select your file');
-        }
-    }
-
-    // form submit
-    const handlePdfFileSubmit = (e) => {
-        e.preventDefault();
-        if (pdfFile !== null) {
-            setViewPdf(pdfFile);
-        }
-        else {
-            setViewPdf(null);
-        }
-    }
-
-
     const [isPageLoading, setPageLoading] = useState(true);
     const [programList, setProgramList] = useState();
 
@@ -313,42 +263,6 @@ function AddContent({
                                         onBlur={handleBlur}
                                     />
 
-                                    // <Pdf />
-                                    // <Pdf2 />
-
-                                    // <div className='container'>
-                                    //     <br></br>
-                                    //     <form className='form-group' onSubmit={handlePdfFileSubmit}>
-                                    //         <InputComponent type="file" className='form-control'
-                                    //             required onChange={handleChange} // respon yg diberikan akan berbeda
-                                    //             name="contents"
-                                    //             value={values.contents}
-                                    //             // onChange={handleChange}
-                                    //             // onClick={handlePdfFileChange}
-                                    //             // uda bisa, tinggal bagaimana url pdf dari computer terbaca oleh push sehingga bisa terlihat hasil file yang di upload
-                                    //             // pemanggilan pdf seperti apa
-                                    //         />
-                                            
-                                    //         {pdfFileError && <div className='error-msg'>{pdfFileError} </div>}
-                                    //         <br></br>
-                                    //         <button type="submit" className='btn btn-success btn-lg' onClick={handlePdfFileSubmit} >
-                                    //             UPLOAD
-                                    //         </button>
-                                    //     </form>
-                                    //     <br></br>
-                                    //     <h4>View PDF</h4>
-                                    //     <div className='pdf-container'>
-                                    //         {/* show pdf conditionally (if we have one)  */}
-                                    //         {viewPdf && <><Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
-                                    //             <Viewer fileUrl={viewPdf}
-                                    //                 plugins={[defaultLayoutPluginInstance]} 
-                                    //                />
-                                    //         </Worker></>}
-
-                                    //         {/* if we dont have pdf or viewPdf state is null */}
-                                    //         {!viewPdf && <>No pdf file selected</>}
-                                    //     </div>
-                                    // </div>
                                 ) : null}
                                 {touched.contents && <span className="message__error">{errors.contents}</span>}
                             </div>
