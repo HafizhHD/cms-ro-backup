a86b6fa70ad402127d2428910259f57706cf69ae
nafislaila <nafislaila@nafislailas-MacBook-Pro.local>
2022-02-23 15:12:26 +0700

    api

--- src/components/API/urlApi.js
+++ src/components/API/urlApi.js
@@ -1,5 +1,6 @@
 const urlApi =
-    "https://rk.defghi.biz.id:8080/api";
+    // "https://rk.defghi.biz.id:8080/api";
+    "https://as01.prod.ruangortu.id:8080/api"
     //add more url here
 
 export default urlApi;
