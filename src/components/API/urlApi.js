4f7700b17e520cfe30938cf786840131be2b9d5f
nafislaila <nafislaila@nafislailas-MacBook-Pro.local>
2022-02-23 15:18:12 +0700

    apicont

--- src/components/API/urlApi.js
+++ src/components/API/urlApi.js
@@ -1,6 +1,7 @@
 const urlApi =
     // "https://rk.defghi.biz.id:8080/api";
     "https://as01.prod.ruangortu.id:8080/api"
+    //pergantian api baru - lebih cepat load content
     //add more url here
 
 export default urlApi;
