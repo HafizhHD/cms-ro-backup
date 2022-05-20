c93d2700e632742b3a7b24da7009da9bb280836e
hafizhdary <hafizh.dary@defghi.id>
2022-05-20 13:00:01 +0700

    fix menu

--- src/components/Navigations/Navigations.js
+++ src/components/Navigations/Navigations.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, {useState} from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 import './Navigations.scss';
 import {FaWhatsapp} from 'react-icons/fa'
+import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
 
 function Navigations({ MenuItems, ShowAside }) {
+    const currentLocation = useLocation();
+    console.log(currentLocation);
+
+    const [isActive, setActive] = useState({path: '', active: false});
+
     return (
         <aside className="Aside">
             <ul className="Aside__Nav">
@@ -27,30 +33,48 @@ function Navigations({ MenuItems, ShowAside }) {
                                     }}>
                                         {item.icon}
                                         <p>{item.pathName}</p>
+                                        {isActive.path === item.path && isActive.active === true ? (
+                                            <FiChevronUp />
+                                        ) : <FiChevronDown />}
                                     </NavLink>
                                 ) :
                             (
                                 <>
-                                    <NavLink to={item.path} className="Aside__Nav_item-disabled" onClick={() => {
-                                        
+                                    <NavLink to={currentLocation.pathname} className="Aside__Nav_item-disabled" onClick={() => {
+                                        {
+                                            var pairActive = {}
+                                            const itemString = item.path;
+                                            if(isActive.path != itemString)
+                                                pairActive = {path: itemString, active: true};
+                                            else {
+                                                if(isActive.active === true) pairActive = {path: itemString, active: false};
+                                                else pairActive = {path: itemString, active: true};
+                                            }
+                                            setActive(pairActive);
+                                        }
                                     }}>
                                         {item.icon}
                                         <p>{item.pathName}</p>
+                                        {isActive.path === item.path && isActive.active === true ?
+                                            (<FiChevronUp className="Aside__Nav_item-disabled-icon"/>) :
+                                            (<FiChevronDown className="Aside__Nav_item-disabled-icon"/>)}
                                     </NavLink>
-                                    {item.children.map((x, idx) => {
-                                        console.log(x.path);
-                                        return (
-                                            <li key={item.path + idx} className="Aside__Nav_item">
-                                                <NavLink to={item.path + x.path} className="Aside__Nav_item-children" onClick={() => {
-                                                    window.scroll(0,0);
-                                                    if(window.innerWidth < 900) ShowAside(false);
-                                                }}>
-                                                    {x.icon}
-                                                    <p>{x.pathName}</p>
-                                                </NavLink>
-                                            </li>
-                                        )
-                                    })}
+                                    {isActive.path === item.path && isActive.active === true ? 
+                                        item.children.map((x, idx) => {
+                                            console.log(x.path);
+                                            
+                                            return (
+                                                <li key={item.path + idx} className="Aside__Nav_item">
+                                                    <NavLink to={item.path + x.path} className="Aside__Nav_item-children" onClick={() => {
+                                                        window.scroll(0,0);
+                                                        if(window.innerWidth < 900) ShowAside(false);
+                                                    }}>
+                                                        {x.icon}
+                                                        <p>{x.pathName}</p>
+                                                    </NavLink>
+                                                </li>
+                                            )
+                                        }) : null}
                                 </>
                             )}
                         </li>
